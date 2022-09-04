a5310e8345694c0e33ba30555ae98be41773e243
Sahil <112234265+amicable-sahil@users.noreply.github.com>
2022-09-04 11:12:04 +0530

    Add files via upload

---
+++ Assignment_09.py
@@ -0,0 +1,87 @@
+#Q1:----------------------------------------------------
+n = int(input("Enter a number "))
+i = 1
+while i<=n:
+    print("MySirG", end=" ")
+    i +=1
+
+#Q2:----------------------------------------------------
+n = int(input("Enter a number "))
+i = 1
+while i<=n:
+    print(i, end=" ")
+    i +=1
+
+#Q3:-----------------------METHOD 1-----------------------------
+n = int(input("Enter a number "))
+i = 0
+while i<n:
+    print(n-i, end=" ")
+    i +=1
+
+#----------------------------METHOD 2-------------------------
+n = int(input("Enter a number "))
+i = 0
+while n>i:
+    print(n, end=" ")
+    n -=1
+
+
+#Q4:-------------------METHOD 1----------------------------
+n = int(input("Enter a number "))
+i = 1
+while i<=2*n:
+    print(i, end=" ")
+    i +=2
+
+#------------------------METHOD 2----------------------------
+n = int(input("Enter a number "))
+i = 0
+while i<n:
+    print(2*i+1, end=" ")
+    i +=1
+
+
+#Q5:-----------------------------------------------
+n = int(input("Enter a number "))
+i = 1
+while i<=2*n:
+    print(2*n-i, end=" ")
+    i +=2
+
+
+#Q6:-----------------------------------------------
+n = int(input("Enter a number "))
+i = 1
+while i<=n:
+    print(2*i, end=" ")
+    i +=1
+
+
+#Q7:-----------------------------------------------
+n = int(input("Enter a number "))
+i = 0
+while i < 2*n:
+    print(2*n-i, end=" ")
+    i += 2
+
+
+#Q8:-------------------------------------------------
+n = int(input("Enter a number "))
+i=1
+while(i<=n):
+    print(i**2, end=" ")
+    i +=1
+
+#Q9:-------------------------------------------------
+n = int(input("Enter a number "))
+i=1
+while(i<=n):
+    print(i**3, end=" ")
+    i +=1
+
+#Q10:-------------------------------------------------
+i=1
+while(i<=10):
+    print(i*10, end=" ")
+    i +=1
