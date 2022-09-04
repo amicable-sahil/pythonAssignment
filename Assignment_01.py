499d3a52d37c7f85bb0f99d884c596e828b7e08c
Sahil <112234265+amicable-sahil@users.noreply.github.com>
2022-09-04 11:10:15 +0530

    Add files via upload

---
+++ Assignment_01.py
@@ -0,0 +1,57 @@
+""" 
+Q1: Python Verson
+Ans: I used [ py ] in  command prompt to see the version of the python 
+"""
+# ---------Assignment 1 -> Question 1-------------------
+# import sys
+# print(sys.version)
+# ------------------------------------------------------
+
+"""    
+Q2: Python script to print Hello Python on the screen in cmd.
+Ans: I wrote [ Print "Hello Python" ] cmd 
+"""
+
+# Q3: Script store in a file and run from command line
+print("I'll be execute from cmd")
+# I give the path in cmd and my programme runs efficiently.
+
+# Q4:
+print("Hello Python")
+
+# Q5:
+print("Hello", "python", sep="\n")  # Method 1
+print("Hello \npython")  # Method 2
+
+# Q6:
+print("MySirG")
+
+# ---------Assignment 1 -> Question 6-------------------
+#print("\"MySirG\"")   <- It will print "MySirG" with quotes
+# ------------------------------------------------------
+
+# Q7:
+print("Teacher's Day")
+
+
+# Q8:
+name = "Sahil"
+print(name)
+
+# Q9:
+name = "Sahil"
+age = 30
+qualification = "B.A. Graduate"
+experience = "1 Year in Frontend Developing"
+print(name, age, qualification, experience, sep="\n")
+
+
+# Q10:  
+a = 10
+b = 20
+c = 30
+print(a, b, c, sep="\n")
+
+
+# Here I'm assigning a variable to use in a different module (02_Assignment)
+varOfAssignment1 = "I am the value of Assignment 01"
