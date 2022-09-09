6b0bec582f5252b09b0183db5d08fab0fbf7dc9f
Sahil <112234265+amicable-sahil@users.noreply.github.com>
2022-09-09 17:42:08 +0530

    Add files via upload

---
+++ Assignment_17.py
@@ -0,0 +1,54 @@
+# Q1:---------------------------------------------------------------
+s1 = {"Python", "Java", "JavaScript", "C", "php", "React"}
+print(s1)
+
+# Q2:---------------------------------------------------------------
+s1 = {"name: Sahil", "Gender: Male", "Age: 30", "Qualification: BA Greduate", "City: Hanumangarh(Rajasthan)"}
+print(s1)
+
+
+# Q3:---------------------------------------------------------------
+s1 = {"Python", "Java", "JavaScript", "C", "php", "React"}
+print(type(s1))
+
+
+# Q4:---------------------------------------------------------------
+s1 = {"Java", "Python", "Django"}
+print("Python" in s1)
+
+
+# Q5:---------------------------------------------------------------
+s1 = {"Python", "Java", "JavaScript", "C", "php", "React"}
+s2 = {"Java", "Angular", "Django", "Cpp", "NoSQL"}
+s1.update(s2)
+print(s1)
+
+
+# Q6:---------------------------------------------------------------
+s1 = {"Python", "Java", "JavaScript", "C", "php", "React"}
+s2 = ["Java", "Angular", "Django", "Cpp", "NoSQL"]
+s1.update(s2)
+print(s1)
+
+# Q7:---------------------------------------------------------------
+s1 = {"Python", "Dango", "JavaScript", "SQL"}
+print(s1)
+print(s1.pop())
+
+
+# # Q8:---------------------------------------------------------------
+s1 = {"Python", "Dango", "JavaScript", "SQL"}
+del s1
+print(s1)
+
+# Q9:---------------------------------------------------------------
+s1 = {"Python", "Dango", "JavaScript", "SQL"}
+{ print(e, end=" ") for e in s1 }
+
+
+# Q10:---------------------------------------------------------------
+s1 = {20, 56, 24, 15, 80, 25, 45, 60, 20}
+print(min(s1))
+print(max(s1))
+
+
